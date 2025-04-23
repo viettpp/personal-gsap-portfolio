d604d29d2bf9f9ee9885295a71963e1579573005
Viet Pham <viet@duck.com>
2025-04-23 15:18:12 +0200

    Comment out NavigationDock import in Home page for future implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero"
 import About from "@/components/About"
 import Services from "@/components/Services"
 import Contact from "@/components/Contact"
-import NavigationDock from "@/components/NavigationDock"
+{/* import NavigationDock from "@/components/NavigationDock" */} // Uncomment this line to enable the Navigation Dock
 
 const Home = () => {
   return (
