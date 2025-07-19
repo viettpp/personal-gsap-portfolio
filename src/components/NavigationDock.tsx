292619e951cc596cc7e21216546989d39e4a9211
Viet Pham <viet@duck.com>
2025-07-19 11:42:45 +0200

    Update NavigationDock component: change background color to a semi-transparent gray for improved aesthetics.

--- src/components/NavigationDock.tsx
+++ src/components/NavigationDock.tsx
@@ -178,7 +178,7 @@ export default function NavigationDock() {
       ref={dockRef} // Attach ref here
       className={cn(
         "fixed bottom-[3rem] left-1/2 -translate-x-1/2 flex items-center justify-center rounded-md shadow-lg overflow-hidden z-[100]",
-        "backdrop-blur-md bg-white/20", // Frosted glass effect and bottom class to control space to the bottom of the screen
+        "backdrop-blur-md bg-[#A4A4A418]", // Frosted glass effect and bottom class to control space to the bottom of the screen
         "w-[80vw] max-w-[15rem] xl:max-w-[16.35rem] 2xl:max-w-[21.8rem]",
         "h-[2.6rem] xl:h-[2.83rem] 2xl:h-[3.78rem]" // Dock size
       )}
